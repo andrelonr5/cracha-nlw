21a6f79aedf47d39585f9b00c0ac1edfc5b49ddd
Andrelon Brito <50185628+andrelonr5@users.noreply.github.com>
2021-10-23 12:53:55 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+const LinksSocialMidia = {
+  github: 'andrelonr5',
+  youtube: 'https://www.youtube.com/channel/UC33UAH30BteoNcApoGXwvaA',
+  facebook: 'andrelon.brito',
+  instagram: 'andre_lonbrito',
+  twitter: 'andrelonbrito'
+}
+function chageSocialMdediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+
+    li.children[0].href = `https://${social}.com/${LinksSocialMidia[social]}`
+  }
+}
+chageSocialMdediaLinks()
+
+function getGitHubProfileInfos() {
+  const url = `https://api.github.com/users/${LinksSocialMidia.github}`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userLink.href = data.html_url
+      UserImage.src = data.avatar_url
+      userLogin.textContent = data.login
+    })
+}
+
+getGitHubProfileInfos()
