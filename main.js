2db9c5a3cacb147e3f1986050de0bb11c54c1f0d
Andrelon Brito <50185628+andrelonr5@users.noreply.github.com>
2022-03-10 20:51:56 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const LinksSocialMidia = {
   github: 'andrelonr5',
-  youtube: 'channel/UC33UAH30BteoNcApoGXwvaA',
+  youtube: 'channel/',
   facebook: 'andrelon.brito',
   instagram: 'andre_lonbrito',
   twitter: 'andrelonbrito'
